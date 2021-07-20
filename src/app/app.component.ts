e7e6524012ae96f40c57477f3a38a475bd08d6b8
Mary Ngarachu <m.ngarachu@carepay.local>
2021-07-20 17:28:36 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+ // styleUrls: ['./app.component.css']
+  styles: [`
+  h1 {
+    color: red;
+    font-size: 25px;
+  }
+  
+  `]
+})
+export class AppComponent {
+
+}
